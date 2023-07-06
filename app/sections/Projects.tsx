0599e61f24d510fd434d31d1cc8de5a0ae02a6a3
Gama Kusumah <gamakusumah@gmail.com>
2023-07-06 15:54:42 +0700

    add new project and change Tailor On url demo

--- app/sections/Projects.tsx
+++ app/sections/Projects.tsx
@@ -18,14 +18,14 @@ export default function Projects() {
       </div>
 
       {/* Project Card */}
-      <div className="grid gap-14 px-2 md:gap-16 md:px-4 lg:gap-20 lg:px-8">
+      <div className="grid gap-14 px-2 md:gap-16 md:px-4 lg:gap-28 lg:px-8">
         <ProjectCard
           name="NFT Marketplace"
           designer="Anima"
           designerLink="https://www.figma.com/community/file/1173962104946517060"
           link="https://nft-marketplace-one-zeta.vercel.app/"
           desc="NFT Marketplace UI Created With Anima For Figma. Collect, Buy And Sell Art From More Than 20k Nft Artists."
-          tools={["NextJS", "Tailwind", "Framer Motion"]}
+          tools={["Next.js", "Tailwind", "Framer Motion"]}
           cta="View Demo"
           imgSrc="/projects/nft.png"
           caption={null}
@@ -44,16 +44,28 @@ export default function Projects() {
           imgOrder={3}
         />
         <ProjectCard
+          name="Jasa Prima Admin"
+          designer="SAYA"
+          designerLink="/"
+          link="https://drive.google.com/file/d/1gu3JstvVqo-DDpuVmCYiq9SPCFn20FxE/view0"
+          desc="A Jasa Prima Admin Dashboard for administrative management of office and orders data."
+          tools={["React.js", "TailwindCSS", "MongoDB", "Express.js"]}
+          cta="View Demo"
+          imgSrc="/projects/jpd2.png"
+          caption={null}
+          imgOrder={null}
+        />
+        <ProjectCard
           name="TAILOR ON"
           designer="SAYA"
           designerLink="/"
-          link="https://youtu.be/SSDJvXbhRnQ"
+          link="https://drive.google.com/file/d/1YDtU5tnZoUes1xYHXppRnuYhdhQKhsbu/view"
           desc="Tailor On is a custom clothing website where customers can choose the model, fabric and size they want and supported by Auto-Generate Pattern technology."
           tools={["Php", "Bootstrap", "MySql"]}
           cta="View Demo"
           imgSrc="/projects/tailoron.png"
           caption={null}
-          imgOrder={null}
+          imgOrder={3}
         />
       </div>
     </section>
